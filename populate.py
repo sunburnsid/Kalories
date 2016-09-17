e9add797c97eef15c7ffd53a16a9aeaf46c9c124
Shrey <sj335@cornell.edu>
2016-09-17 17:49:45 -0400

    imported from db

--- populate.py
+++ populate.py
@@ -28,15 +28,17 @@ def importCSV():
     db.create_all()
     with open('ABBREV.csv', 'rb') as csvfile:
         reader=csv.reader(csvfile)
-        x=0
+        #x=0
+        reader.next() #skip header
         for row in reader:
-            if (x<5):
+            #if (x<5):
                 #print (row[1]+" "+ row[4]+" "+ row[7]+ row[5]+ row[10]+row[33]+row[31]+row[20]+row[43]+row[3]+row[49])
-                x+=1
+                #x+=1
             #Format: name, protein, carbs, fat, calcium, vitA, vitB, vitC, vitK, healthy, calories, unit
-                temp=Food(row[1], float(row[4]), row[7], row[5], row[10],row[33],row[31],row[20],row[43],True,row[3],row[49])
-                db.session.add(temp)
-                db.session.commit()
+            #temp=Food(row[1], float(row[4]), float(row[7]), float(row[5]), float(row[10]),float(row[33]),float(row[31]),float(row[20]),float(row[43]),True,float(row[3]),row[49])
+            temp=Food(row[1], row[4], row[7], row[5], row[10],row[33],row[31],row[20],row[43],True,row[3],row[49])
+            db.session.add(temp)
+            db.session.commit()
 
 if __name__ == '__main__':
 	#initiate()
