d780009ec4a60b43005c85f75dd3338066d2a928
Shrey <sj335@cornell.edu>
2016-09-17 12:29:47 -0400

    Populate database

--- kalories.py
+++ kalories.py
@@ -42,22 +42,32 @@ class Food(db.Model):
         self.calories = calories
         self.unit     = unit
 
-class API(db.model):
+class API(db.Model):
     __tablename__ = 'api'
     date = db.Column(db.Integer)
-    food = db.Column(db.Integer, ForeignKey(food.name))
+    food = db.Column(db.Integer) #ForeignKey("food.name"))
     quantity = db.Column(db.Integer)
     url = db.Column(db.String)
 
-    def __init__(self, date, food, quantity, url)
+    def __init__(self, date, food, quantity, url):
+        self.date=date
+        self.food=food
+        self.quantity=quantity
+        self.url=url
 
+@app.route('/upload')
+#import one time
+def uploadcsv():
+    with open('ABBREV.csv', 'rb') as csvfile:
+        reader=csv.reader(csvfile)
+        for row in reader:
+            print row
 
 @app.route('/')
 def show_all():
     return render_template('show_all.html',
         food=Food.query.order_by(Food.id.desc()).all()
     )
-
 
 @app.route('/new', methods=['GET', 'POST'])
 def new():
@@ -95,7 +105,7 @@ def getSuggestions(base_id):
     #clarifai_api = ClarifaiApi() # assumes environment variables are set.
     #result = clarifai_api.tag_images(open(dest, 'rb'))
 
-    result = {u'status_code': u'OK', u'status_msg': u'All images in request have co$
+    result = {u'status_code': u'OK', u'status_msg': u'All images in request have completed successfully. ', u'meta': {u'tag': {u'timestamp': 1474084104.627829, u'model': u'general-v1.3', u'config': None}}, u'results': [{u'docid': 42024272502422257284519421165328554557L, u'status_code': u'OK', u'status_msg': u'OK', u'local_id': u'', u'result': {u'tag': {u'classes': [u'carrot', u'root', u'food', u'no person', u'provitamin A', u'vegetable', u'nutrition', u'health', u'healthy', u'agriculture', u'diet', u'leaf', u'desktop', u'freshness', u'grow', u'close-up', u'juicy', u'bunch', u'ingredients', u'cooking'], u'concept_ids': [u'ai_DGvpjM5g', u'ai_3xJvggfW', u'ai_3PlgVmlN', u'ai_786Zr311', u'ai_grVmTvp7', u'ai_LpcgM7r5', u'ai_nl2sV1Hm', u'ai_mZ2tl6cW', u'ai_0mCQLwrm', u'ai_ZsprRgCn', u'ai_Cp0N9mF9', u'ai_wBCrHPDb', u'ai_Lq00FggW', u'ai_mZJ4nprg', u'ai_VRrpDkps', u'ai_4lvjn8qv', u'ai_gt6djlDq', u'ai_mXdcXbrT', u'ai_xzqBDlmV', u'ai_KXNqVd5F'], u'probs': [0.9975664615631104, 0.9955800175666809, 0.9922435283660889, 0.99013352394104, 0.9898305535316467, 0.9868329763412476, 0.9771294593811035, 0.9762943983078003, 0.9708182215690613, 0.9601856470108032, 0.956566572189331, 0.9558433294296265, 0.9373021125793457, 0.9226887226104736, 0.9224319458007812, 0.9132283329963684, 0.911675214767456, 0.9096507430076599, 0.9050804972648621, 0.8930140733718872]}}, u'docid_str': u'1f9d949a5c38fffe3e7e2f58c7ca5a3d'}]}
     parsed = result['results'][0]['result']['tag']['classes']
     answer = []
     result = {u'status_code': u'OK', u'status_msg': u'All images in request have completed successfully. ', u'meta': {u'tag': {u'timestamp': 1474084104.627829, u'model': u'general-v1.3', u'config': None}}, u'results': [{u'docid': 42024272502422257284519421165328554557L, u'status_code': u'OK', u'status_msg': u'OK', u'local_id': u'', u'result': {u'tag': {u'classes': [u'carrot', u'root', u'food', u'no person', u'provitamin A', u'vegetable', u'nutrition', u'health', u'healthy', u'agriculture', u'diet', u'leaf', u'desktop', u'freshness', u'grow', u'close-up', u'juicy', u'bunch', u'ingredients', u'cooking'], u'concept_ids': [u'ai_DGvpjM5g', u'ai_3xJvggfW', u'ai_3PlgVmlN', u'ai_786Zr311', u'ai_grVmTvp7', u'ai_LpcgM7r5', u'ai_nl2sV1Hm', u'ai_mZ2tl6cW', u'ai_0mCQLwrm', u'ai_ZsprRgCn', u'ai_Cp0N9mF9', u'ai_wBCrHPDb', u'ai_Lq00FggW', u'ai_mZJ4nprg', u'ai_VRrpDkps', u'ai_4lvjn8qv', u'ai_gt6djlDq', u'ai_mXdcXbrT', u'ai_xzqBDlmV', u'ai_KXNqVd5F'], u'probs': [0.9975664615631104, 0.9955800175666809, 0.9922435283660889, 0.99013352394104, 0.9898305535316467, 0.9868329763412476, 0.9771294593811035, 0.9762943983078003, 0.9708182215690613, 0.9601856470108032, 0.956566572189331, 0.9558433294296265, 0.9373021125793457, 0.9226887226104736, 0.9224319458007812, 0.9132283329963684, 0.911675214767456, 0.9096507430076599, 0.9050804972648621, 0.8930140733718872]}}, u'docid_str': u'1f9d949a5c38fffe3e7e2f58c7ca5a3d'}]}
@@ -130,7 +140,8 @@ def confirmFood():
         vitamins.add(values["vitamins"])
         healthy=values["healthy"]
     return flask.jsonify(protein=protein,carbs=carbs, fat=fat,fiber=fiber,
-    calcium=calcium, vitamins=vitamins, healthy=healthy, calories=calories)
+    calcium=calcium, vitamins=vitamins, healthy=healthy, calories=calories,
+    url=content['url'])
 
 @app.route('/getDay/<int:day>', methods = ['GET'])
 def giveDay(day):
@@ -143,4 +154,4 @@ def giveDay(day):
 
 
 if __name__ == '__main__':
-app.run()
+    app.run()
